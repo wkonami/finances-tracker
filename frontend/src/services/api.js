1530476ce67018e82b4900fd688c4196e40d68d0
Willian Kaname <kanamewillian@gmail.com>
2026-05-03 19:22:09 -0400

    login fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -23,24 +23,4 @@ api.interceptors.request.use((config) => {
 
 });
 
-api.interceptors.response.use(
-
-  (response) => response,
-
-  (error) => {
-
-    if (error.response?.status === 401) {
-
-      localStorage.removeItem('token');
-
-      window.location.href = '/';
-
-    }
-
-    return Promise.reject(error);
-
-  }
-
-);
-
 export default api;
