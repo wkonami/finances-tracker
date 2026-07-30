ee9361fc7cc934e95776d11658f186eefce50f7e
Willian Kaname <kanamewillian@gmail.com>
2026-07-30 15:58:03 -0400

    fix(routes): corrige import do auth middleware

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const authMiddleware = require('../middlewares/auth');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const roleMiddleware = require('../middlewares/role');
 
